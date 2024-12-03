978df5d3aca0c4fc4a1aa2b592c021b9ca266285
booyaalex <64170386+booyaalex@users.noreply.github.com>
2024-12-03 18:05:19 +0000

    testing

--- src/engine.js
+++ src/engine.js
@@ -1,5 +1,5 @@
 "use strict";
-import * as THREE from 'three';
+import * as THREE from '/workspaces/Game/node_modules/@types/three/build/three.module';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
