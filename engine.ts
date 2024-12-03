fd73258fa78d7e1ee1373fe19ab7c891f113582e
booyaalex <64170386+booyaalex@users.noreply.github.com>
2024-12-03 18:00:38 +0000

    I got something holy crap

--- engine.ts
+++ engine.ts
@@ -6,3 +6,18 @@ const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera( 75, window.
 const renderer:THREE.WebGLRenderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+
+const geometry: THREE.BoxGeometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube: THREE.Mesh = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+function animate(): void {
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+	renderer.render( scene, camera );
+}
+renderer.setAnimationLoop( animate );
